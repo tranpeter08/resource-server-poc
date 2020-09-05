b8c2f682c2e13c3013ed87eb02854ed23115d211
Peter Tran <tran.peter08@gmail.com>
2020-09-05 02:20:54 -0700

    moved auth0 config to its own file

--- src/config.js
+++ src/config.js
@@ -3,19 +3,5 @@ const jwksRsa = require('jwks-rsa');
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  auth0JwtConfig: {
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: process.env.AUTH0_JWKS_URI,
-    }),
-    audience: process.env.AUTH0_CLIENT_ID,
-    issuer: process.env.AUTH0_ISSUER,
-    algorithms: ['RS256'],
-  },
-  oktaJwtConfig: {
-    audience: process.env.OKTA_CLIENT_ID,
-    issuer: process.env.OKTA_ISSUER,
-  },
+  AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
 };
